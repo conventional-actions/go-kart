bf0022246f351c0e88152843f731863002bbc1d9
Seth Yates <29878+sethyates@users.noreply.github.com>
2023-01-10 12:09:28 -0800

    chore: upgrade deps (#72)

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,7 +6,7 @@ import {expect, test} from '@jest/globals'
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const ip = path.join(__dirname, '..', 'dist', 'main', 'index.js')
   const options: cp.ExecFileSyncOptions = {
     env: process.env,
     stdio: 'pipe'
